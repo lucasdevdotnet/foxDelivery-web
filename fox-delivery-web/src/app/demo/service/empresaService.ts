8599e681df856e85a8e6c7c62e12c7644d98d03c
lucas <lucas.santana@pdcase.com.br>
2024-11-14 17:59:50 -0300

    Ajustes

--- fox-delivery-web/src/app/demo/service/empresaService.ts
+++ fox-delivery-web/src/app/demo/service/empresaService.ts
@@ -5,30 +5,32 @@ import { Empresa } from '../model/empresa';
 
 @Injectable()
 export class EmpresaService {
-  apiURL = 'https://localhost:44376/v1/api';
-  constructor(private http: HttpClient) { }
+
+    //apiURL = 'https://192.168.1.105/v1/api';
+    apiURL = 'https://localhost:44376/v1/api';
+    constructor(private http: HttpClient) { }
 
 
-  getEmpresas() {  //poste
-    return this.http.get<any>('assets/demo/data.json') //api
-    .toPromise()
-    .then(res => res.data as Empresa [])
-    .then(data => data);
-  }
-
-  adicionar (empresa: Empresa): any {
-
-   debugger
-  return this.http.post(`${this.apiURL }/Empresa`, empresa)
-  .subscribe(
-    resultado => {
-      console.log(resultado)
-    },
-    erro => {
-      if(erro.status == 400) {
-        console.log(erro);
-      }
+    getEmpresas() {  //poste
+        return this.http.get<any>('assets/demo/data.json') //api
+            .toPromise()
+            .then(res => res.data as Empresa[])
+            .then(data => data);
     }
-  );
-}
+
+    adicionar(empresa: Empresa): any {
+
+        debugger
+        return this.http.post(`${this.apiURL}/Empresa`, empresa)
+            .subscribe(
+                resultado => {
+                    console.log(resultado)
+                },
+                erro => {
+                    if (erro.status == 400) {
+                        console.log(erro);
+                    }
+                }
+            );
+    }
 }
